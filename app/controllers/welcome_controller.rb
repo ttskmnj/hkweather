ce65598df2b272d29d23dc8e639690258f8f1a30
monji <monjindhu@gmail.com>
2015-06-05 13:33:19 +0800

    simple modify

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
+require 'net/http'
+
 class WelcomeController < ApplicationController
 	def index
+		uri = URI('http://api.openweathermap.org/data/2.5/forecast')
+        params = {:q => 'hongkong'}
+        uri.query = URI.encode_www_form(params)
+
+        test = Net::HTTP.get(uri)
+		
+        render plain:test
 	end
 end
