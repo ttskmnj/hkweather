804ac59f77243d6e858169b38d9b35c37f3cb66a
monji <monjindhu@gmail.com>
2015-06-10 00:28:33 +0800

    test0 add route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/scavenge' => 'scavenge#index'
   get '/hkweather' => 'hkweather#index'
-  # Example of regular route:
+  # Example of regular route: 
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
