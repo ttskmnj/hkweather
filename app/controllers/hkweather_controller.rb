2b399002b6071b39967b155b9fff1df88fb575ae
monji <monjindhu@gmail.com>
2015-06-10 12:27:08 +0800

    test0 add fix scavenge

--- app/controllers/hkweather_controller.rb
+++ app/controllers/hkweather_controller.rb
@@ -1,5 +1,13 @@
 class HkweatherController < ApplicationController
 	def index
+		test = '2015-06-11'
+		weather = HkWeather.where('date >= ?',test).order('date ASC').limit(4)
+
+		render json:ActiveSupport::JSON.encode(weather)
+
+	end
+
+	def weatherapi
 =begin
 		test = '2015-06-11'
 		weather = HkWeather.where('date >= ?',test).order('date ASC').limit(4)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/scavenge' => 'scavenge#index'
   get '/hkweather' => 'hkweather#index'
+  post '/hkweather' => 'hkweather#weatherapi'
   # Example of regular route: 
   #   get 'products/:id' => 'catalog#view'
 
