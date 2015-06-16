0ce143bf6328ec51dabad128ba2163846bc8ca38
monji <monjindhu@gmail.com>
2015-06-16 22:17:05 +0800

    dbg

--- app/controllers/hkweather_controller.rb
+++ app/controllers/hkweather_controller.rb
@@ -1,6 +1,8 @@
 class HkweatherController < ApplicationController
 	skip_before_filter :verify_authenticity_token
-	before_filter :set_headers
+	before_filter :cors_preflight_check
+	after_filter :cors_set_access_control_headers
+	
 
 	def index
 		today = Time.new
@@ -13,14 +15,4 @@ class HkweatherController < ApplicationController
 
 		render json: weather
 	end
-
-  private
-
-  def set_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Expose-Headers'] = 'ETag'
-    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-    headers['Access-Control-Max-Age'] = '86400'
-  end
 end
