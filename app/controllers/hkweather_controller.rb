aaac0083db0e7b8e1389d19b7dbc5b9bfe35e2bb
monji <monjindhu@gmail.com>
2015-06-16 22:19:05 +0800

    dbg

--- app/controllers/hkweather_controller.rb
+++ app/controllers/hkweather_controller.rb
@@ -1,8 +1,5 @@
 class HkweatherController < ApplicationController
 	skip_before_filter :verify_authenticity_token
-	before_filter :cors_preflight_check
-	after_filter :cors_set_access_control_headers
-	
 
 	def index
 		today = Time.new
