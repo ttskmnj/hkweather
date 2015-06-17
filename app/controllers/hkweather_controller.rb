498231c5d2cbe0e9a560dbc15af9ce95b1438bea
monji <monjindhu@gmail.com>
2015-06-17 11:18:44 +0800

    add front-end

--- app/controllers/hkweather_controller.rb
+++ app/controllers/hkweather_controller.rb
@@ -2,8 +2,15 @@ class HkweatherController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 
 	def index
+		@date = Hash.new
 		today = Time.new
 		@weather = HkWeather.where('date >= ?',today.localtime.to_date).order('date ASC').limit(4)
+		@weather.each_index do |k|
+			date = @weather[k]['date'].mday
+			month = @weather[k]['date'].mon
+
+			@date[k] =  "#{month}/#{date}"
+		end
 	end
 
 	def weatherapi
