78d3ae3f0f0ebdc0b0ba8e7a0af2567f4b5184f1
Tatsuki Monji <monji@mojitachijus-MacBook-Air.local>
2015-10-29 17:42:11 +0900

    modify route again

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'hkweather#index'
   get '/scavenge' => 'scavenge#index'
-  get '/' => 'hkweather#index'
+  get '/hkweather' => 'hkweather#index'
   post '/hkweather' => 'hkweather#weatherapi'
   # Example of regular route: 
   #   get 'products/:id' => 'catalog#view'
